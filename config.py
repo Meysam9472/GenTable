798252cbab7994d88824f3b6aed3f7301661abcf
misammehmannavaz <misammehmannavaz@gmail.com>
2026-05-06 23:17:56 +0330

    Add time table and users apps

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from routers import time_table, users
 
 app = FastAPI()
+
+app.include_router(time_table.router, tags=["time_table"])
+app.include_router(users.router, tags=["users"])
