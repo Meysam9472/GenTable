1f45ee845239c7c6443da3bb3a311dbb6d319c85
Meysam9472 <meysammehmannavaz94@gmail.com>
2026-05-19 17:59:14 +0330

    Implement time_table_maker_task

--- apps/time_table_maker/tasks.py
+++ apps/time_table_maker/tasks.py
@@ -1,3 +1,8 @@
 from services.time_table_service import time_table_maker
 from celery import shared_task
 
+
+@shared_task(bind=True, max_retries=5)
+def time_table_maker_task(self, teachers_data, courses_data, num_rooms, cohorts, days, hours):
+    result = time_table_maker(teachers_data, courses_data, num_rooms, cohorts, days, hours)
+    return result
