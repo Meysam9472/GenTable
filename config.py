dffd42d2f8943c682bf0daf911f6c210919f4568
Meysam9472 <meysammehmannavaz94@gmail.com>
2026-05-20 18:45:16 +0330

    Fix a bug

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from routers import time_table, users
+from apps.time_table_maker import time_table
+from apps.users import manage_users
+
 
 app = FastAPI()
 
 app.include_router(time_table.router, tags=["time_table"])
-app.include_router(users.router, tags=["users"])
+app.include_router(manage_users.router, tags=["users"])
