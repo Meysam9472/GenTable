7845312e6bfef0575f8db0ed52997556678b1fc8
Meysam9472 <meysammehmannavaz94@gmail.com>
2026-05-25 17:53:58 +0330

    Remove redundant module

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from apps.time_table_maker import time_table
-from apps.users import manage_users
+from apps.users import users
 
 
 app = FastAPI()
 
 app.include_router(time_table.router, tags=["time_table"])
-app.include_router(manage_users.router, tags=["users"])
+app.include_router(users.router, tags=["users"])
